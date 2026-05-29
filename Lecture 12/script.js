61254e2acce6dc5328ca37953dd6b3ad2b8b0731
Prem Kumar Shaw <277076306+premkumarshaw04@users.noreply.github.com>
2026-05-29 16:13:31 +0530

    Initial commit

---
+++ Lecture 12/script.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function(){
+    alert("Hello Ji Kaise ho saare"); //when you execute the file. then with the execution a message will pop up from title baar
+});
